371c6e8577175dec1ab445235eee1f57e5d7be19
hwang <hwangchang01@naver.com>
2018-11-20 11:59:29 +0900

    랜덤이미지2개

--- oh.py
+++ oh.py
@@ -1,11 +1,12 @@
 import pygame, sys
 import random
+import threading
 
 White = (255,255,255)
 wid = 512
 hei = 512
 
-
+'''
 def cannot_out():
     global x,y
     if x < 0:
@@ -35,33 +36,45 @@ def moving():
                 y_change = 0
         x += x_change
         y += y_change
-
+'''
 
 pygame.init()
 display = pygame.display.set_mode((wid, hei))
+display.fill(White)
 pygame.display.set_caption("how_to_move")
 
-imagename="C:/Users/user/Pictures/testimage"+(str)(random.randrange(0,3))+".png"
-print(imagename)
-frog = pygame.image.load(imagename) # 사진파일
+imagename1="C:/Users/user/Pictures/testimage"+(str)(random.randrange(0,3))+".png"
+frog1 = pygame.image.load(imagename1) # 사진파일
+frog1 = pygame.transform.scale(frog1, (100,100))
+x1 = wid/2-50
+y1 = hei/2-50
 
-frog = pygame.transform.scale(frog, (wid,hei))
-y_change = 0
-x_change = 0
-x = wid/2-wid/2
-y = hei/2-wid/2
+def printfrog1():
+    global frog1,x1,y1
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+        display.blit(frog1, (x1, y1))
+
+thread_recv = threading.Thread(target=printfrog1, args=())
+thread_recv.start()
+
+imagename2="C:/Users/user/Pictures/testimage"+(str)(random.randrange(0,3))+".png"
+frog2 = pygame.image.load(imagename2) # 사진파일
+frog2 = pygame.transform.scale(frog2, (100,100))
+x2 = 50
+y2 = 50
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
-        moving()
-        cannot_out()
-
     pygame.display.update()
-    display.fill(White)
-    display.blit(frog, (x, y))
+    display.blit(frog2, (x2, y2))
+
 
 #  https://blog.naver.com/rsj0908/221007425974  에서 가져옴
