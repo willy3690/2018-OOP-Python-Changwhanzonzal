09133ca7c4109ab8000e3d1a81df40364087352c
willy <willy3690@sasa.hs.kr>
2018-11-22 14:57:26 +0900

    backspace 가능

--- pygame_event.py
+++ pygame_event.py
@@ -16,7 +16,6 @@ PURPLE=(127,0,127)
 size = [400, 300]
 screen = pygame.display.set_mode(size)
 font = pygame.font.SysFont("consolas", 20)
-
 pygame.display.set_caption("Ocean View SOS game!")
 
 # Loop until the user clicks the close button.
@@ -33,20 +32,26 @@ def printText(msg, color='BLACK', pos=(50, 50)):
 
     screen.blit(textSurface, textRect)
 
-texty=""
-chktexty=0
+texty = ""
+chktexty = 0
 
 while not done:
 
     # This limits the while loop to a max of 10 times per second.
     # Leave this out and we will use all CPU we can.
-    clock.tick(10)
+    #clock.tick(10000)
 
     # Main Event Loop
     for event in pygame.event.get():  # User did something
         if event.type == pygame.KEYDOWN:  # If user release what he pressed.
             pressed = pygame.key.get_pressed()
             buttons = [pygame.key.name(k) for k, v in enumerate(pressed) if v]
+            if buttons[0] == 'backspace':
+                if len(texty) >= 1:
+                    texty = texty[0:len(texty)-1]
+                continue
+            elif len(buttons[0]) > 1:
+                continue
             flag = True
         elif event.type == pygame.KEYUP:  # If user press any key.
             flag = False
@@ -63,22 +68,24 @@ while not done:
 
     # Print red text if user pressed any key.
     if flag == True:
-        if len(texty)==4: texty=""
+        if len(texty) >= 4:
+            texty = ""
         printText('you just key down!!', 'RED')
         printText('--> you pressed any key.', 'RED', (50, 70))
-        printText('Pressed Key : ' + buttons[0], 'RED', (50, 90))
-        if chktexty==0: texty+=buttons[0]
-        chktexty=1
+        for i in buttons:
+            printText('Pressed Key : ' + i, 'RED', (50, 90))
+            if chktexty == 0: texty += i
+            chktexty=1
 
-    if len(texty)==4:
-        if(texty=='frog'):
+    if len(texty) == 4:
+        if texty =='frog':
             printText('What the hells going on?', 'BLACK', (50, 155))
         else:
             printText('Not Frog!', 'BLACK', (50, 155))
 
     # Print blue text if user released any key.
     if flag == False:
-        chktexty=0
+        chktexty = 0
         printText('you just key up!!', 'BLUE')
         printText('--> released what you pressed.', 'BLUE', (50, 70))
 
