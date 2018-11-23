4461ae59f2d82fe0c37134ad7007f3c005a677bc
hwang <hwangchang01@naver.com>
2018-11-23 22:34:13 +0900

    11_23_오후 10시 반 이걸 돌리렴

--- 20181122manyimages.py
+++ 20181122manyimages.py
@@ -3,6 +3,9 @@ import random
 import threading
 import time
 
+def printimage(image):
+    display.blit(image.frog,(image.x,image.y))
+
 White = (255,255,255)
 wid = 800
 hei = 512
@@ -10,7 +13,7 @@ hei = 512
 pygame.init()
 display = pygame.display.set_mode((wid, hei))
 display.fill(White)
-pygame.display.set_caption("how_to_move")
+pygame.display.set_caption("해상구조 SOS")
 
 class Charactor:
     def __init__(self,inputx,inputy,wid,hei):
@@ -42,24 +45,22 @@ char1=Otherimage(80,350,100,100,"C:/Users/user/Desktop/pyproimage/char2.png")
 
 onimage=Tube(10,400,240,100)
 
-def displayblit(image):
-    display.blit(image.frog,(image.x,image.y))
-
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     pygame.display.update()
-    time.sleep(0.1)
-    displayblit(wallpaper)
-    displayblit(onimage)
-    displayblit(char1)
-    '''
-    display.blit(wallpaper.frog, (wallpaper.x, wallpaper.y))
-    display.blit(onimage.frog, (onimage.x, onimage.y))
-    display.blit(char1.frog, (char1.x, char1.y))
-    '''
+    time.sleep(0.0001)
+    display.fill(White)
+    printimage(wallpaper)
+    printimage(onimage)
+    printimage(char1)
+    move=[2, -2, 0]
+    ymove = move[random.randrange(0, 3)]
+    xmove = move[random.randrange(0, 3)]
+    if char1.x+xmove<90 and char1.x+xmove>70: char1.x+=xmove
+    if char1.y + ymove < 360 and char1.y + ymove > 340: char1.y+=ymove
 
 #  https://blog.naver.com/rsj0908/221007425974  에서 가져옴
 # https://pixlr.com/editor/ 에서 이미지 수정
