f24af90d97c83bfe75c2bf30e459b86e259e0848
willy <willy3690@sasa.hs.kr>
2018-11-25 23:28:54 +0900

    튜브 구현

--- text_moving.py
+++ text_moving.py
@@ -31,7 +31,7 @@ pygame.display.set_caption("해상구조 SOS")
 texty = ""
 input_word = ""
 pressed_button = list()
-vel_plus = 0.1
+vel_plus = 0.01
 
 class item:
     def __init__(self, inputx, inputy, w, h):
@@ -50,6 +50,7 @@ class Tube:
         self.frog = pygame.transform.scale(self.frog, (w, h))
         self.x = inputx
         self.y = inputy
+        self.word = random.choice(wordlist)
 
 
 class Otherimage:
@@ -61,14 +62,26 @@ class Otherimage:
         self.y = inputy
 
 
-wallpaper=Otherimage(0,212,800,300,"pyproimage/wallpaper.png")
-boom=Otherimage(-50,100,100,100,"pyproimage/boom.png")
-char1=Otherimage(80, 350, 100, 100, "pyproimage/char2.png")
-onimage = Tube(10, 400, 240, 100)
+wallpaper = Otherimage(0, 212, 800, 300, "pyproimage/wallpaper.png")
+boom = Otherimage(-50, 100, 100, 100, "pyproimage/boom.png")
+char1 = Otherimage(200, 200, 100, 100, "pyproimage/char2.png")
+onimage = Tube(130, 250, 240, 100)
 item1 = item(800, 100, 100, 100)
-
+tube1 = Tube(0, 400, 240, 100)
+tube2 = Tube(200, 400, 240, 100)
+tube3 = Tube(400, 400, 240, 100)
+tube4 = Tube(600, 400, 240, 100)
 score = float(0)
-itemvel = 2
+itemvel = 1
+tube_word = [tube1.word, tube2.word, tube3.word, tube4.word]
+
+
+def del_tube(temp1, temp2):
+    t = Tube(temp1, 400, 240, 100)
+    tube_word[temp2] = t.word
+    return t
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
@@ -89,13 +102,28 @@ while True:
                         texty = texty[0:len(texty) - 1]
                     continue
                 elif buttons[i] == 'return':
+                    chk = False
                     if texty == item1.word:
                         itemvel += vel_plus
                         score += 1
                         item1 = item(800, 100, 100, 100)
-                    input_word = texty
+                        chk = True
+                    if not chk:
+                        for j in range(4):
+                            if texty == tube_word[j]:
+                                score += 1
+                                if j == 0:
+                                    tube1 = del_tube(0, 0)
+                                    break
+                                elif j == 1:
+                                    tube2 = del_tube(200, 1)
+                                    break
+                                elif j == 2:
+                                    tube3 = del_tube(400, 2)
+                                    break
+                                else:
+                                    tube4 = del_tube(600, 3)
                     texty = ""
-                    random.shuffle(wordlist)
                     continue
                 elif buttons[0] == 'space':
                     texty += ' '
@@ -115,8 +143,15 @@ while True:
     printimage(onimage)
     printimage(char1)
     printimage(item1)
-    printText(item1.word, pos=(item1.x + 3 * len(item1.word), item1.y + 100))
-    printText(wordlist[0],"black",(300,512))
+    printimage(tube1)
+    printimage(tube2)
+    printimage(tube3)
+    printimage(tube4)
+    printText(tube1.word, color= "White", pos=(tube1.x + 70, tube1.y + 50))
+    printText(tube2.word, color= "White", pos=(tube2.x + 70, tube2.y + 50))
+    printText(tube3.word, color= "White", pos=(tube3.x + 70, tube3.y + 50))
+    printText(tube4.word, color= "White", pos=(tube4.x + 70, tube4.y + 50))
+    printText(item1.word, pos=(item1.x + 20, item1.y + 100))
     printText('Please enter the word')
     printText(texty, "black", (0, 532))
     printText('Score: ' + str(round(score, 1)), "black", (0, 0))
