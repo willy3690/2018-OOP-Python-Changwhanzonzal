7fcae8a7e7b12c068f0f033a63cc1f0a337b3616
willy <willy3690@sasa.hs.kr>
2018-12-03 22:05:38 +0900

    레벨 출력

--- alltubelist.py
+++ alltubelist.py
@@ -1,4 +1,5 @@
 import random, time, math, sys, pygame
+
 pygame.init()
 fp = open('pyprowords.txt', 'r')
 wordlist = []
@@ -7,14 +8,14 @@ for line in fp:
 
 
 def printimage(image):
-    display.blit(image.frog,(image.x, image.y))
+    display.blit(image.frog, (image.x, image.y))
 
 
 def printText(msg, color='BLACK', pos=(0, 512), infon=0):
     textSurface = font[infon].render(msg, True, pygame.Color(color), None)
     textRect = textSurface.get_rect()
     textRect.topleft = pos
-    display.blit(textSurface,textRect)
+    display.blit(textSurface, textRect)
 
 
 White = (255, 255, 255)
@@ -50,10 +51,10 @@ start_time_enitem = time.time()
 
 class item:
     def __init__(self, inputx, inputy, w=80, h=80):
-        self.itemnum=random.randrange(0,7)+1
-        self.imagename = "pyproimage/image"+(str)(self.itemnum)+".png"
-        self.frog = pygame.image.load(self.imagename) # 사진파일
-        self.frog = pygame.transform.scale(self.frog, (w,h))
+        self.itemnum = random.randrange(0, 7) + 1
+        self.imagename = "pyproimage/image" + (str)(self.itemnum) + ".png"
+        self.frog = pygame.image.load(self.imagename)  # 사진파일
+        self.frog = pygame.transform.scale(self.frog, (w, h))
         self.x = inputx
         self.y = inputy
         self.word = random.choice(wordlist)
@@ -61,8 +62,8 @@ class item:
 
 class Tube:
     def __init__(self, inputx, inputy, w=240, h=100):
-        self.imagename="pyproimage/tubevector.png"
-        self.frog = pygame.image.load(self.imagename) # 사진파일
+        self.imagename = "pyproimage/tubevector.png"
+        self.frog = pygame.image.load(self.imagename)  # 사진파일
         self.frog = pygame.transform.scale(self.frog, (w, h))
         self.x = inputx
         self.y = inputy
@@ -70,7 +71,7 @@ class Tube:
 
 
 class Charac:
-    def __init__(self,inputx, inputy, w=100, h=100):
+    def __init__(self, inputx, inputy, w=100, h=100):
         self.charnum = random.randrange(1, 3) + 1
         self.imagename = "pyproimage/char" + (str)(self.charnum) + ".png"
         self.frog = pygame.image.load(self.imagename)  # 사진파일
@@ -81,7 +82,7 @@ class Charac:
 
 class Otherimage:
     def __init__(self, inputx, inputy, w, h, imaname):
-        self.imagename=imaname
+        self.imagename = imaname
         self.frog = pygame.image.load(self.imagename)  # 사진파일
         self.frog = pygame.transform.scale(self.frog, (w, h))
         self.x = inputx
@@ -98,10 +99,10 @@ pause_im1 = Otherimage(0, 0, wid, hei, "pyproimage/test_rule.png")
 play_image = Otherimage(750, 0, 50, 50, "pyproimage/Play.png")
 
 # char1,2는 각각 나와 컴퓨터의 캐릭터이며, x와 y좌표를 인자로 받는다.
-char1 = Charac(200,270)
-char2 = Charac(570,270)
-charlist=[char1,char2]
-tube_under_list=[]
+char1 = Charac(200, 270)
+char2 = Charac(570, 270)
+charlist = [char1, char2]
+tube_under_list = []
 
 
 def check_use(word):
@@ -118,27 +119,30 @@ def check_use(word):
         if changed:
             break
     return word
+
+
 # 단어가 적혀있는 튜브들의 리스트
 
 
 for i in range(4):
-    temp = Tube(200*i, 400)
+    temp = Tube(200 * i, 400)
     temp.word = check_use(temp.word)
     tube_under_list.append(temp)
 
 # 나와 컴퓨터의 튜브 리스트이다.
 # tube_upper_list[0] 리스트에는 내 튜브 인스턴스가,
 # tube_upper_list[1] 리스트에는 상대 튜브 인스턴스가 들어있다.
-tube_upper_list=[[], []]
+tube_upper_list = [[], []]
 
-flag=False
+flag = False
 
 score = float(0)
 pause_image = Otherimage(750, 0, 50, 50, "pyproimage/Pause.png")
 itemvel = 2
-sinx=[0,0.8]
+sinx = [0, 0.8]
 rep = False
 is_start = True
+lev = 1
 
 
 def shiver():
@@ -146,24 +150,25 @@ def shiver():
     global sinx
     for i in range(2):
         for tubes in tube_upper_list[i]:
-            tubes.y+=0.5*math.sin(sinx[i])
-            tubes.x+=0.2*math.sin(sinx[i]*2)
-        charlist[i].y+=0.5*math.sin(sinx[i])
-        charlist[i].x+=0.2*math.sin(sinx[i]*2)
-    item1.y+=0.3*math.sin(sinx[i])
+            tubes.y += 0.5 * math.sin(sinx[i])
+            tubes.x += 0.2 * math.sin(sinx[i] * 2)
+        charlist[i].y += 0.5 * math.sin(sinx[i])
+        charlist[i].x += 0.2 * math.sin(sinx[i] * 2)
+    item1.y += 0.3 * math.sin(sinx[i])
     for tubes in tube_under_list:
-        tubes.y+=0.15 * math.sin(sinx[i])
-    for i in range(2): sinx[i]+=0.04
+        tubes.y += 0.15 * math.sin(sinx[i])
+    for i in range(2): sinx[i] += 0.04
 
 
 # more이 0(tube_upper_list의 index!)이면 내 쪽에, 1이면 상대쪽에 튜브를 쌓는 함수
 def stacktube(more):
-    xlist=[130, 500]
+    xlist = [130, 500]
     if len(tube_upper_list[more]) == 0:
         new_player_tube = Tube(xlist[more], 300)
-    else: new_player_tube = Tube(xlist[more], tube_upper_list[more][len(tube_upper_list[more]) - 1].y - 30)
+    else:
+        new_player_tube = Tube(xlist[more], tube_upper_list[more][len(tube_upper_list[more]) - 1].y - 30)
     tube_upper_list[more].append(new_player_tube)
-    charlist[more].y -=30
+    charlist[more].y -= 30
 
 
 def poptube(more):
@@ -183,7 +188,7 @@ for i in range(2):
 
 # 아이템의 번호num=(item1.itemnum)와 0 또는 1의 more를 입력받는다.
 # more가 0이면 내가 num에 해당하는 아이템을, 1이면 상대가 먹은 것.
-def itemeffect(num,more):
+def itemeffect(num, more):
     global item1
     global tube_under_list
     global tube_upper_list
@@ -197,7 +202,7 @@ def itemeffect(num,more):
         start_time_pop[more] += 3
     if num == 3:
         for i in range(len(tube_upper_list[more])): poptube(more)
-        start_time_pop[more]=time.time()
+        start_time_pop[more] = time.time()
     if num == 4:
         pass
     if num == 5:
@@ -211,8 +216,8 @@ def itemeffect(num,more):
 while True:
     pygame.display.update()
     display.fill(White)
-    printText("해상구조대 SOS", pos=(wid/2-180, hei / 2 - 120), infon=4)
-    printimage(Otherimage(wid/2-100, hei/2-10, 200, 200, "pyproimage/Play.png"))
+    printText("해상구조대 SOS", pos=(wid / 2 - 180, hei / 2 - 120), infon=4)
+    printimage(Otherimage(wid / 2 - 100, hei / 2 - 10, 200, 200, "pyproimage/Play.png"))
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if is_start and wid / 2 - 100 <= event.pos[0] < wid / 2 + 100 and hei / 2 - 10 <= event.pos[1] <= hei / 2 + 190:
@@ -247,7 +252,7 @@ while True:
                     if texty == item1.word:
                         itemvel += vel_plus
                         score += 1
-                        itemeffect(item1.itemnum,0)
+                        itemeffect(item1.itemnum, 0)
                         chk = True
                     if not chk:
                         for j in range(4):
@@ -268,13 +273,13 @@ while True:
                     texty = texty + buttons[i]
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if rep and hei * 3 / 4 - 100 <= event.pos[1] <= hei * 3 / 4:
-                if wid/3-50 <= event.pos[0] <= wid/3+50:    #  초기화
+                if wid / 3 - 50 <= event.pos[0] <= wid / 3 + 50:  # 초기화
                     rep = False
                     texty = ""
                     pressed_button = []
                     score = 0
                     continue
-                elif wid*2/3-50 <= event.pos[0]:
+                elif wid * 2 / 3 - 50 <= event.pos[0]:
                     pygame.quit()
                     sys.exit()
             if event.pos[0] >= 750 and event.pos[1] <= 50:
@@ -298,18 +303,18 @@ while True:
         start_time_enitem = 2 * time.time() - start_time_enitem
         continue
 
-    if time.time()-start_time_enitem >= delta_t_enitem:
-        #컴퓨터가 아이템을 먹는 부분
-        itemeffect(item1.itemnum,1)
-        start_time_enitem=time.time()
+    if time.time() - start_time_enitem >= delta_t_enitem:
+        # 컴퓨터가 아이템을 먹는 부분
+        itemeffect(item1.itemnum, 1)
+        start_time_enitem = time.time()
 
-    if time.time()-start_time_entube >= delta_t_entube:
-        #컴퓨터가 튜브를 먹는 부분
-        change=random.randrange(0,4)
-        tube_under_list[change]=Tube(tube_under_list[change].x,tube_under_list[change].y)
-        tube_under_list[change].word=check_use(tube_under_list[change].word)
+    if time.time() - start_time_entube >= delta_t_entube:
+        # 컴퓨터가 튜브를 먹는 부분
+        change = random.randrange(0, 4)
+        tube_under_list[change] = Tube(tube_under_list[change].x, tube_under_list[change].y)
+        tube_under_list[change].word = check_use(tube_under_list[change].word)
         stacktube(1)
-        start_time_entube=time.time()
+        start_time_entube = time.time()
 
     if time.time() - start_time_pop[0] >= delta_t_pop[0]:
         poptube(0)
@@ -320,17 +325,21 @@ while True:
         start_time_pop[1] = time.time()
     for i in range(2):
         if len(tube_upper_list[i]) >= 7:
-            tube_upper_list[i] = [tube_upper_list[i][0],tube_upper_list[i][1]]
-            delta_t_pop[i] -= 0.5
-            charlist[i].y = 200
-            start_time_pop[i] = time.time()
+            if i == 0:
+                tube_upper_list[i] = [tube_upper_list[i][0], tube_upper_list[i][1]]
+                delta_t_pop[i] -= 0.5
+                charlist[i].y = 200
+                start_time_pop[i] = time.time()
+                lev += 1
+            else:
+                rep = True
     if rep:
         pygame.display.update()
         display.fill(White)
-        printText("Game end", pos=(wid/2-75, hei/2-215), infon=3)
-        printText("Your score {}, replay?".format(score), pos=(wid/2-185, hei/3*2-150), infon=3)
+        printText("Game end", pos=(wid / 2 - 75, hei / 2 - 215), infon=3)
+        printText("Your score {}, replay?".format(round(score, 2)), pos=(wid / 2 - 185, hei / 3 * 2 - 150), infon=3)
         printimage(Otherimage(wid / 3 - 50, hei * 3 / 4 - 100, 100, 100, "pyproimage/yes.png"))
-        printimage(Otherimage(wid/3*2-50, hei*3/4-100, 100, 100, "pyproimage/no.png"))
+        printimage(Otherimage(wid / 3 * 2 - 50, hei * 3 / 4 - 100, 100, 100, "pyproimage/no.png"))
         continue
 
     pygame.display.update()
@@ -338,11 +347,12 @@ while True:
     printimage(wallpaper)
 
     for i in range(2):
-        printText('pop:'+(str)((int)(5-(time.time()-start_time_pop[i]))), "Black", (charlist[i].x+27, charlist[i].y-20), 2)
+        printText('pop:' + (str)((int)(5 - (time.time() - start_time_pop[i]))), "Black",
+                  (charlist[i].x + 27, charlist[i].y - 20), 2)
 
     for tubes in tube_under_list: printimage(tubes)
     for i in range(4):
-        printText(tube_under_list[i].word, color= "White", pos=(tube_under_list[i].x + 70, tube_under_list[i].y + 50))
+        printText(tube_under_list[i].word, color="White", pos=(tube_under_list[i].x + 70, tube_under_list[i].y + 50))
 
     shiver()
 
@@ -362,7 +372,7 @@ while True:
     printText('Please enter the word')
     printText(texty, "black", (0, 532))
     printText('Score: ' + str(round(score, 1)), "black", (0, 0))
-
+    printText('Level: ' + str(lev), "black", (0, 20))
     if item1.x > -100:
         item1.x -= itemvel
         if item1.x < -96:
